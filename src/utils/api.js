86a50c3e570f3f69096da698a3758ba7f5b11c45
SuperDino <serzh.bakalov.81@mail.ru>
2023-05-06 15:37:15 +0400

    F-03 изменения

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 export const apiCreate = axios.create({
   baseURL: 'https://api.football-data.org/v2/',
-  headers: { 'X-Auth-Token': '57cfaf485b6645c69da1e555276e4bd8' },
+  headers: { 'X-Auth-Token': 'dd127c2af68b49d9a207d889c5c7f97e' },
 });
