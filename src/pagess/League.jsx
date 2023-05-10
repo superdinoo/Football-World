087c89f958dc16c7bfb3e6deee11f8a67b6cc629
SuperDino <serzh.bakalov.81@mail.ru>
2023-05-10 23:17:10 +0400

    Настройка

---
+++ src/pagess/League.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from '../components/Header';
+import BordsLeft from '../components/BordsLeft';
+import BordsRight from '../components/BordsRight';
+
+
+import Footer from '../components/Footer';
+
+
+
+
+const League = () => {
+return (
+<React.Fragment>
+<Header />
+<BordsLeft />
+<BordsRight />
+
+
+
+<Footer />
+</React.Fragment>
+)
+}
+export default League;
