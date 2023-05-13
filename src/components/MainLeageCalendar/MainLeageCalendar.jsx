d193667bb5470d938b22c57acfb69b1b0bd31c91
SuperDino <serzh.bakalov.81@mail.ru>
2023-05-13 13:51:34 +0400

    f-10 верстка страницы календаря лиг

--- src/components/MainLeageCalendar/MainLeageCalendar.jsx
+++ src/components/MainLeageCalendar/MainLeageCalendar.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React , { Fragment, useState, useEffect } from 'react';
 import BordsLeft from '../BordsLeft';
+import TableComp from '../TableComp';
 import BordsRight from '../BordsRight';
 import style from './MainLeageCalendar.module.css';
+
 
 const MainLeageCalendar = () => {
   return (
     <div className={style.mainleage}>
       <BordsLeft />
-    
+      <TableComp />
       <BordsRight />
     </div>
   );
